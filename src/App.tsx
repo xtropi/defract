8b1042c34a89a5b61ecc520b1a020bf8f262bbc7
Mukhtar Musaev <mukhtarmusaev@yandex.ru>
2022-02-04 02:38:23 +0300

    eslint added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './styles.css'
 import { ClickCounter } from './ClickCounter'
 
 export const App = () => {
+    const num = "asdasd"
     return <>
     <h1>Hello world! {process.env.name} {process.env.NODE_ENV}</h1>
     <ClickCounter/>
