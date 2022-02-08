95339ea3d8c48a82d76aabfd5b14ace83829127e
Mukhtar Musaev <mukhtarmusaev@yandex.ru>
2022-02-08 04:11:02 +0300

    crypto-js added

--- src/pages/Authentication.tsx
+++ src/pages/Authentication.tsx
@@ -1,8 +1,28 @@
 import { Button, Grid } from '@mui/material'
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
+import { SHA256, AES, enc } from 'crypto-js'
+import { useState } from 'react'
 
 export function Authentication() {
+  const [input, setInput] = useState()
+
+  function handleClick() {
+    console.log('button pressed')
+  }
+
+  function handleChange(event: any) {
+    const value = event.target.value
+    setInput(value)
+    console.log(SHA256(value).toString())
+    const encrypted = AES.encrypt(value, 'Secret Passphrase').toString()
+    const decrypted = AES.decrypt(encrypted, 'Secret Passphrase').toString(
+      enc.Utf8
+    )
+    console.log(encrypted)
+    console.log(decrypted)
+  }
+
   return (
     <div>
       <Grid
@@ -27,8 +47,14 @@ export function Authentication() {
               fontWeight: 'bold',
             }}
           >
-            <TextField sx={{ m: 1 }} size="small" id="auth" label="Auth key" />
-            <Button sx={{ m: 1 }} variant="contained">
+            <TextField
+              onChange={handleChange}
+              sx={{ m: 1 }}
+              size="small"
+              id="auth"
+              label="Auth key"
+            />
+            <Button onClick={handleClick} sx={{ m: 1 }} variant="contained">
               Enter
             </Button>
           </Box>
