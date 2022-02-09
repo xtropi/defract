1c455ea52f8233861a04cc7306a7a3b10676f924
Mukhtar Musaev <mukhtarmusaev@yandex.ru>
2022-02-09 05:03:40 +0300

    webpack prettified, firebase config example added

---
+++ src/firebase/firebase.config.example.ts
@@ -0,0 +1,13 @@
+/*
+
+### Get from https://firebase.google.com/
+
+export const firebaseConfig = {
+  apiKey: 'apiKey',
+  authDomain: 'authDomain.firebaseapp.com',
+  projectId: 'projectId',
+  storageBucket: 'storageBucket.appspot.com',
+  messagingSenderId: 'messagingSenderId',
+  appId: 'appIdf',
+}
+*/
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,8 +2,8 @@ const { merge } = require('webpack-merge')
 const commonConfig = require('./webpack.common')
 
 module.exports = (envVars) => {
-    const { env } = envVars
-    const envConfig = require(`./webpack.${env}.js`)
-    const config = merge(commonConfig, envConfig)
-    return config
+  const { env } = envVars
+  const envConfig = require(`./webpack.${env}.js`)
+  const config = merge(commonConfig, envConfig)
+  return config
 }
