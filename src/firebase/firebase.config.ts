8caee332c2c4b8041f6763b585a831cc37116d46
Mukhtar Musaev <mukhtarmusaev@yandex.ru>
2022-02-09 04:29:58 +0300

    config removed

--- src/firebase/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyDzVUczF37vkUQ_nhoRZJKJWWNfQViO7eo',
-  authDomain: 'defract-6b97c.firebaseapp.com',
-  projectId: 'defract-6b97c',
-  storageBucket: 'defract-6b97c.appspot.com',
-  messagingSenderId: '7000654046',
-  appId: '1:7000654046:web:fee27df09c9052f2d0c24f',
-}
